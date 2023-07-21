6912b294c1ceb3fcd9ac435bd658c6171f889ffb
mustafa1390 <mustafa1390@gmail.com>
2023-07-21 20:47:07 +0330

    L5-Swagger v1

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Api\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+// test
 /*
 |--------------------------------------------------------------------------
 | API Routes
